09f6027bc012fbd0b62594485304a0aeef7e9934
Kirill Müller <krlmlr+r@mailbox.org>
2019-08-22 10:02:20 +0200

    Remove

--- travis/drop_tables.R
+++
@@ -1,9 +0,0 @@
-library(DBI)
-
-devtools::install()
-
-pg <- dbConnect(odbc::odbc(), "PostgreSQL")
-lapply(dbListTables(pg), dbRemoveTable, conn = pg)
-
-mysql <- dbConnect(odbc::odbc(), "MySQL")
-lapply(dbListTables(mysql), dbRemoveTable, conn = mysql)
